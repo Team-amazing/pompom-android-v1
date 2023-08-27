d458009c40baabb8153e830c3df34eadd53f80eb
parkUiery <parkuiery@gmail.com>
2023-08-27 21:17:34 +0900

    🌱 :: 시작화면 기능

---
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/activity/StartActivity.java
@@ -0,0 +1,26 @@
+package com.amazing.android.autopompomme.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amazing.android.autopompomme.databinding.ActivityStartBinding;
+
+public class StartActivity extends AppCompatActivity {
+
+    ActivityStartBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityStartBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnStart.setOnClickListener(v -> {
+            Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
+}
