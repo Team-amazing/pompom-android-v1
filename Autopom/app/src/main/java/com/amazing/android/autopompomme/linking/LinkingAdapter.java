8438aea452d2bdb3e368d92fd3ed164c3d90403a
parkUiery <parkuiery@gmail.com>
2023-09-01 22:13:17 +0900

    🌱 :: 블루투스 리사이클러뷰 어댑터

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/linking/LinkingAdapter.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/linking/LinkingAdapter.java
@@ -1,2 +1,67 @@
-package com.amazing.android.autopompomme.linking;public class LinkingAdapter {
+package com.amazing.android.autopompomme.linking;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.amazing.android.autopompomme.R;
+
+import java.util.ArrayList;
+
+public class LinkingAdapter extends RecyclerView.Adapter<LinkingAdapter.ViewHolder> {
+    private ArrayList<String> list;
+
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView deviceName;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            deviceName = itemView.findViewById(R.id.tv_linking_device);
+        }
+        public TextView getTextView() {
+            return deviceName;
+        }
+
+    }
+
+    public LinkingAdapter (ArrayList<String> dataSet) {
+        Log.d("TEST","t/"+dataSet);
+        list = dataSet;
+    }
+
+    @NonNull
+    @Override
+    public LinkingAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.linking_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LinkingAdapter.ViewHolder holder, int position) {
+        String text = list.get(position);
+        holder.deviceName.setText(text);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+
+
 }
