1bc6a001911b589e79b78274029d791abf5ad35f
parkUiery <parkuiery@gmail.com>
2023-09-03 22:45:31 +0900

    🌱 :: viewPager

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/main/fragment/HomeFragment.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/main/fragment/HomeFragment.java
@@ -1,66 +1,91 @@
 package com.amazing.android.autopompomme.main.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.amazing.android.autopompomme.R;
+import com.amazing.android.autopompomme.databinding.FragmentHomeBinding;
+import com.amazing.android.autopompomme.main.home.MyPlantAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    FragmentHomeBinding binding;
+    private MyPlantAdapter plantAdapter;
+    private ViewPager2 viewPager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        binding = FragmentHomeBinding.inflate(inflater);
+
+        viewPager = binding.vpHome;
+        plantAdapter = new MyPlantAdapter(this);
+
+        int pagerWidth = getResources().getDimensionPixelOffset(R.dimen.pagerWidth);
+        int screenWidth = getResources().getDisplayMetrics().widthPixels;
+        int pagerPadding = (int) ((screenWidth - pagerWidth) * 0.5);
+        int offsetPx = (int) ((screenWidth - pagerWidth)* 0.25);
+        float scaleFactor = 0.8f;
+        float alphaFactor = 0.5f;
+
+        viewPager.setPadding(pagerPadding, 0, pagerPadding, 0);
+        viewPager.setClipToPadding(false);
+        viewPager.setClipChildren(false);
+
+        viewPager.setOffscreenPageLimit(1);
+        viewPager.setPageTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                viewPager.setTranslationX(position * offsetPx);
+
+                int currentPagePosition = viewPager.getCurrentItem();
+                Log.d("TEST","pos"+currentPagePosition);
+                Log.d("TEST","p"+position);
+
+                float scaleFactorAbs = Math.abs(scaleFactor - Math.abs(position));
+                float alpha = Math.max(alphaFactor, scaleFactorAbs);
+
+                if (position == currentPagePosition - 1 || position == currentPagePosition + 1) {
+                    //page.setVisibility(View.INVISIBLE);
+
+
+                    page.setScaleX(1.0f);
+                    page.setScaleY(1.0f);
+                    page.setAlpha(1);
+
+                } else {
+                    page.setVisibility(View.VISIBLE);
+
+
+                    page.setScaleX(scaleFactor);
+                    page.setScaleY(scaleFactor);
+                    page.setAlpha(alpha);
+
+
+                }
+            }
+        });
+
+        viewPager.setAdapter(plantAdapter);
+
+        return binding.getRoot();
     }
 }
