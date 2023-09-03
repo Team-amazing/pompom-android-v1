12af2f40b76e09650a9891f31b18f01d8a13e133
parkUiery <parkuiery@gmail.com>
2023-09-03 22:45:58 +0900

    🌱 :: viewPager 화면 추가

---
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/main/home/MyPlantAdapter.java
@@ -0,0 +1,37 @@
+package com.amazing.android.autopompomme.main.home;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.amazing.android.autopompomme.main.fragment.HomeFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyPlantAdapter extends FragmentStateAdapter {
+
+    private final List<Fragment> fragments;
+
+    public MyPlantAdapter(HomeFragment fa) {
+        super(fa);
+
+        fragments = new ArrayList<>();
+        fragments.add(new AddPlantFragment());
+        fragments.add(new MyPlantFragment());
+        Log.d("TEST","in");
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
+}
