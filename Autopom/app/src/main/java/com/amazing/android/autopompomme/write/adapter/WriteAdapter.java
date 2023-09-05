8e27b67d335b6f95ed0f98f9ad803348008a15ff
parkUiery <parkuiery@gmail.com>
2023-09-05 20:26:46 +0900

    🌱 :: 게시글 작성 사진 리사이클러뷰

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/write/adapter/WriteAdapter.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/write/adapter/WriteAdapter.java
@@ -1,2 +1,61 @@
-package com.amazing.android.autopompomme.write.adapter;public class WriteAdapter {
+package com.amazing.android.autopompomme.write.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.amazing.android.autopompomme.R;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class WriteAdapter extends RecyclerView.Adapter<WriteAdapter.ViewHolder> {
+
+    private Context context;
+    private List<String> imgUrls;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.iv_write_img);
+        }
+
+        public ImageView getImageView() {
+            return imageView;
+        }
+    }
+
+    public WriteAdapter (Context context, List<String> imgUrls){
+        this.context = context;
+        this.imgUrls = imgUrls;
+    }
+    @NonNull
+    @Override
+    public WriteAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.write_img_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WriteAdapter.ViewHolder holder, int position) {
+        String imgUrl = imgUrls.get(position);
+
+        Glide.with(context)
+                .load(imgUrl)
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imgUrls.size();
+    }
 }
