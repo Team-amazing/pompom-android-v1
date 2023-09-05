713f9594934d13a76c46432f1f13937f701d3b78
parkUiery <parkuiery@gmail.com>
2023-09-05 20:24:51 +0900

    🌱 :: 게시글 작성 presenter

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/write/presenter/WritePresenter.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/write/presenter/WritePresenter.java
@@ -1,2 +1,73 @@
-package com.amazing.android.autopompomme.write.presenter;public class WritePresenter {
+package com.amazing.android.autopompomme.write.presenter;
+
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WritePresenter implements WriteContract.Presenter{
+    private WriteContract.View view;
+
+    private FirebaseFirestore fireStore;
+    private StorageReference reference;
+
+    public WritePresenter(WriteContract.View view){
+        this.view = view;
+    }
+    @Override
+    public void write(String title, String detail) {
+        //api 코드
+        //이미지 api
+        reference = FirebaseStorage.getInstance().getReference();
+
+        Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
+        StorageReference riversRef = reference.child("images/rivers.jpg");
+
+        riversRef.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+        //text api
+        fireStore = FirebaseFirestore.getInstance();
+        Map<String, Object> post = new HashMap<>();
+        post.put("title","postTitle");
+        post.put("content","postContent");
+
+        fireStore.collection("posts")
+                .add(post)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TEST", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TEST", "Error adding document", e);
+                    }
+                });
+    }
 }
