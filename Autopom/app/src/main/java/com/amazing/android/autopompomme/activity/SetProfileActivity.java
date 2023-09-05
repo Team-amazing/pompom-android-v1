674cef9cf9420fff372a334ce3c744acacda02a7
parkUiery <parkuiery@gmail.com>
2023-09-05 20:20:17 +0900

    ♻ :: 패키지 변경

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/activity/SetProfileActivity.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/activity/SetProfileActivity.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.amazing.android.autopompomme.api.MemberInfo;
+import com.amazing.android.autopompomme.databinding.ActivitySetProfileBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
