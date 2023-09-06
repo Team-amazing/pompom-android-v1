0649785a519d26a9b1601862620b94011f93f8a4
parkUiery <parkuiery@gmail.com>
2023-09-06 22:53:58 +0900

    🌱 :: 다이얼로그 기능 추가

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/linking/LinkingDialog.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/linking/LinkingDialog.java
@@ -1,2 +1,65 @@
-package com.amazing.android.autopompomme.linking;public class LinkingDialog {
+package com.amazing.android.autopompomme.linking;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.amazing.android.autopompomme.activity.LinkingActivity;
+import com.amazing.android.autopompomme.adapter.LinkingAdapter;
+import com.amazing.android.autopompomme.databinding.LinkingDialogBinding;
+
+public class LinkingDialog extends Dialog {
+
+    LinkingDialogBinding binding;
+
+    private TextView deviceName;
+    private TextView connect;
+    private EditText wifiName;
+    private EditText wifiPw;
+
+    private String title;
+
+    Listener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = LinkingDialogBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+  //      getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        deviceName = binding.tvLinkingDialogDevice;
+        connect = binding.tvLinkingDialogConnect;
+        wifiName = binding.etLinkingDialogWifiName;
+        wifiPw = binding.etLinkingDialogWifiPW;
+
+        deviceName.setText(title);
+
+        binding.tvLinkingDialogConnect.setOnClickListener(v -> {
+            if(listener != null) {
+                listener.wifi(wifiName.toString(),wifiPw.toString());
+            }
+        });
+    }
+
+
+    public LinkingDialog(@NonNull Context context, String title, Listener listener) {
+        super(context);
+
+        this.title = title;
+        this.listener = listener;
+    }
+
+
+
 }
