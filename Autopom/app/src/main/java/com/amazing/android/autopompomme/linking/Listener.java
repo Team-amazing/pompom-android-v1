2eb704132f31736765df493d3f262247ac980239
parkUiery <parkuiery@gmail.com>
2023-09-07 22:59:25 +0900

    ♻ :: 예외처리

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/linking/Listener.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/linking/Listener.java
@@ -1,5 +1,7 @@
 package com.amazing.android.autopompomme.linking;
 
+import java.io.IOException;
+
 public interface Listener {
-    void wifi(String wifiName, String wifiPw);
+    void wifi(String wifiName, String wifiPw,String device) throws IOException;
 }
