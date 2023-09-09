53e4c022f0f1fb87034089511609fa1dc1028249
parkUiery <parkuiery@gmail.com>
2023-09-09 18:14:12 +0900

    🌧 :: 이미지 데이터 추가

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/write/presenter/WriteContract.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/write/presenter/WriteContract.java
@@ -1,11 +1,13 @@
 package com.amazing.android.autopompomme.write.presenter;
 
+import android.net.Uri;
+
 public interface WriteContract {
     interface View{
 
     }
 
     interface Presenter{
-        void write(String title, String detail);
+        void write(String title, String detail, Uri uri);
     }
 }
