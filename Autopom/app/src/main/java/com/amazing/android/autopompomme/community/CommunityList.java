9090759e6736776e82843b49aec929a139bbfff5
parkUiery <parkuiery@gmail.com>
2023-09-10 13:10:27 +0900

    🌱 :: 커뮤니티 데이터 리스트 추가

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/community/CommunityList.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/community/CommunityList.java
@@ -1,2 +1,69 @@
-package com.amazing.android.autopompomme.community;public class CommunityList {
+package com.amazing.android.autopompomme.community;
+
+public class CommunityList {
+    private String profileName;
+    private String profile;
+    private String date;
+    private String title;
+    private String content;
+    private int likeNum;
+    private int commentNum;
+
+    public CommunityList() {}
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getLikeNum() {
+        return likeNum;
+    }
+
+    public void setLikeNum(int likeNum) {
+        this.likeNum = likeNum;
+    }
+
+    public int getCommentNum() {
+        return commentNum;
+    }
+
+    public void setCommentNum(int commentNum) {
+        this.commentNum = commentNum;
+    }
 }
