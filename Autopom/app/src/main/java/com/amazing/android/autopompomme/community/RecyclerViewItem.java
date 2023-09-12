13fe078713f3c4e0178d18364cd07acc5cbecf03
parkUiery <parkuiery@gmail.com>
2023-09-12 20:27:43 +0900

    🌱 :: 사진 뷰페이져

---
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/community/RecyclerViewItem.java
@@ -0,0 +1,15 @@
+package com.amazing.android.autopompomme.community;
+
+import android.net.Uri;
+
+public class ViewPagerItem {
+    private Uri postUri;
+
+    public Uri getPostUri() {
+        return postUri;
+    }
+
+    public void setPostUri(Uri postUri) {
+        this.postUri = postUri;
+    }
+}
