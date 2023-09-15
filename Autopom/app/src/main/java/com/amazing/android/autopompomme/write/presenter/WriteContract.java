ba338e788d64915915a9d8571c7c92fa688f034c
parkUiery <parkuiery@gmail.com>
2023-09-15 14:49:36 +0900

    🌧 :: 파라미터 추가

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/write/presenter/WriteContract.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/write/presenter/WriteContract.java
@@ -2,12 +2,14 @@ package com.amazing.android.autopompomme.write.presenter;
 
 import android.net.Uri;
 
+import java.util.List;
+
 public interface WriteContract {
     interface View{
 
     }
 
     interface Presenter{
-        void write(String title, String detail, Uri uri);
+        void write(String profileName, Uri profileUri, String date,String title, String detail, List<Uri> imgUris);
     }
 }
