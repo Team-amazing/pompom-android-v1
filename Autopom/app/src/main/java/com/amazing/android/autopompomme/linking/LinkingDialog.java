1dfc5b690a91be4874e161eebc92005b18ae675c
parkUiery <parkuiery@gmail.com>
2023-09-15 14:48:36 +0900

    🌧 :: 디바이스 추가

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/linking/LinkingDialog.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/linking/LinkingDialog.java
@@ -2,18 +2,15 @@ package com.amazing.android.autopompomme.linking;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
-import com.amazing.android.autopompomme.activity.LinkingActivity;
-import com.amazing.android.autopompomme.adapter.LinkingAdapter;
 import com.amazing.android.autopompomme.databinding.LinkingDialogBinding;
+
+import java.io.IOException;
 
 public class LinkingDialog extends Dialog {
 
@@ -25,8 +22,10 @@ public class LinkingDialog extends Dialog {
     private EditText wifiPw;
 
     private String title;
+    private String device;
 
     Listener listener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,17 +46,22 @@ public class LinkingDialog extends Dialog {
 
         binding.tvLinkingDialogConnect.setOnClickListener(v -> {
             if(listener != null) {
-                listener.wifi(wifiName.toString(),wifiPw.toString());
+                try {
+                    listener.wifi(wifiName.getText().toString(),wifiPw.getText().toString(),device);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
 
 
-    public LinkingDialog(@NonNull Context context, String title, Listener listener) {
+    public LinkingDialog(@NonNull Context context, String title, String device, Listener listener) {
         super(context);
 
         this.title = title;
         this.listener = listener;
+        this.device = device;
     }
 
 
