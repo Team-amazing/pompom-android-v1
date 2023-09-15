5fd819a0cc7193274525fa8b202416b8d421bfc7
parkUiery <parkuiery@gmail.com>
2023-09-15 14:50:15 +0900

    ♻ :: 파이어베이스 데이터 추가 로직 변경

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/write/presenter/WritePresenter.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/write/presenter/WritePresenter.java
@@ -5,70 +5,99 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class WritePresenter implements WriteContract.Presenter{
+public class WritePresenter implements WriteContract.Presenter {
     private WriteContract.View view;
 
     private FirebaseFirestore fireStore;
+    private FirebaseStorage storage;
     private StorageReference reference;
 
-    public WritePresenter(WriteContract.View view){
+    public WritePresenter(WriteContract.View view) {
         this.view = view;
     }
+
     @Override
-    public void write(String title, String detail,Uri uri) {
+    public void write(String profileName, Uri profileUri, String date, String title, String detail, List<Uri> imgUris) {
+
+        List<Task<Uri>> uploadTasks = new ArrayList<>();
+        storage = FirebaseStorage.getInstance();
+        fireStore = FirebaseFirestore.getInstance();
+        for(Uri imgUri : imgUris) {
+            StorageReference imgRef = storage.getReference().child("images/" + imgUri.getLastPathSegment());
+            UploadTask uploadTask = imgRef.putFile(imgUri);
+            Log.d("TEST","task"+uploadTask);
+
+            Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if(!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return imgRef.getDownloadUrl();
+                }
+            });
+
+            uploadTasks.add(uriTask);
+
+        }
+
+        Tasks.whenAllSuccess(uploadTasks).addOnSuccessListener(new OnSuccessListener<List<Object>>() {
+            @Override
+            public void onSuccess(List<Object> list) {
+                List<Uri> imgUriList = new ArrayList<>();
+                for(Object object : list) {
+                    Uri downloadUrl = (Uri) object;
+                    imgUriList.add(downloadUrl);
+                }
+
+                Log.d("TEST","downloadUrl"+imgUriList);
+                Map<String, Object> post = new HashMap<>();
+                post.put("profileName", profileName);
+                post.put("profileUri", profileUri);
+                post.put("date", date);
+                post.put("title", title);
+                post.put("content", detail);
+                post.put("postUri", imgUriList);
+
+                fireStore.collection("posts")
+                        .add(post)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d("TEST", "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w("TEST", "Error adding document", e);
+                            }
+                        });
+
+            }
+        });
+
+        //--------------------------------------------------------------------------------------
+
         //api 코드
         //이미지 api
-        reference = FirebaseStorage.getInstance().getReference();
 
-        //Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
-        StorageReference riversRef = reference.child("images/"+uri.getLastPathSegment());
-
-        riversRef.putFile(uri)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
-                    }
-                });
-
-        //text api
-        fireStore = FirebaseFirestore.getInstance();
-        Map<String, Object> post = new HashMap<>();
-        post.put("title",title);
-        post.put("content",detail);
-        Log.d("TEST","ti"+title);
-
-        fireStore.collection("posts")
-                .add(post)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("TEST", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("TEST", "Error adding document", e);
-                    }
-                });
     }
 }
