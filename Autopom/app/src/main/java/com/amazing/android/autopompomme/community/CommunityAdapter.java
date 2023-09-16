bc5a2dba77925536eac13bc39bc5663af0284847
parkUiery <parkuiery@gmail.com>
2023-09-16 17:01:22 +0900

    🌧 :: 좋아요 서버 연결

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/community/CommunityAdapter.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/community/CommunityAdapter.java
@@ -1,15 +1,9 @@
 package com.amazing.android.autopompomme.community;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,11 +12,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.amazing.android.autopompomme.R;
-import com.amazing.android.autopompomme.community.detail.DetailActivity;
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,11 +36,17 @@ public class CommunityAdapter extends RecyclerView.Adapter<CommunityAdapter.View
     private ArrayList<CommunityList> arrayList;
     private ViewGroup parent;
     private int likeNum;
-    private int likeN;
+    private Timer timer;
+    private boolean isLikePending = false;
+    private boolean isLike = false;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private String postId;
+    private String userId;
+    private String documentId;
 
-
-    public CommunityAdapter(ArrayList<CommunityList> arrayList) {
+    public CommunityAdapter(ArrayList<CommunityList> arrayList,String userId) {
         this.arrayList = arrayList;
+        this.userId = userId;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -91,20 +102,15 @@ public class CommunityAdapter extends RecyclerView.Adapter<CommunityAdapter.View
         holder.tvLike.setText(arrayList.get(position).getLikeNum()+"명이 하트를 보냈어요");
         holder.comment.setText("댓글 "+arrayList.get(position).getCommentNum()+"개");
         likeNum = arrayList.get(position).getLikeNum();
+        postId = arrayList.get(position).getPostId();
 
+        initLike(holder);
         holder.btnNoLike.setOnClickListener( v -> {
-            holder.btnNoLike.setVisibility(View.GONE);
-            holder.btnLike.setVisibility(View.VISIBLE);
-            likeNum += 1;
-            holder.tvLike.setText(likeNum +"이 하트를 보냈어요");
+            noLike(holder);
         });
 
         holder.btnLike.setOnClickListener( v -> {
-            holder.btnNoLike.setVisibility(View.VISIBLE);
-            holder.btnLike.setVisibility(View.GONE);
-            likeNum -= 1;
-            holder.tvLike.setText(likeNum+"이 하트를 보냈어요");
-
+            yseLike(holder);
         });
 
         List<String> imgUri = arrayList.get(position).getPostUri();
@@ -112,6 +118,132 @@ public class CommunityAdapter extends RecyclerView.Adapter<CommunityAdapter.View
         Log.d("TEST","s"+imgUri);
         PostImgAdapter adapter = new PostImgAdapter(imgUri);
         holder.pager.setAdapter(adapter);
+    }
+
+    private void yseLike(ViewHolder holder) {
+        holder.btnNoLike.setVisibility(View.VISIBLE);
+        holder.btnLike.setVisibility(View.GONE);
+        likeNum -= 1;
+        holder.tvLike.setText(likeNum+"명이 하트를 보냈어요");
+        clickBtn();
+        isLike = false;
+    }
+
+    private void noLike(ViewHolder holder) {
+        holder.btnNoLike.setVisibility(View.GONE);
+        holder.btnLike.setVisibility(View.VISIBLE);
+        likeNum += 1;
+        holder.tvLike.setText(likeNum +"명이 하트를 보냈어요");
+        clickBtn();
+        isLike = true;
+    }
+
+    private void initLike(ViewHolder holder) {
+        db.collection("likes")
+                .whereEqualTo("userId",userId)
+                .whereEqualTo("postId", postId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            if(task.getResult().isEmpty()) {
+                                //문서가 없음
+                                makeSever();
+                                Log.d("TEST","문서 없음");
+                            }else {
+                                for(QueryDocumentSnapshot document : task.getResult()) {
+                                    documentId = document.getId();
+                                    isLike = Boolean.TRUE.equals(document.getBoolean("liked"));
+                                }
+                            }
+                        }else {
+                            Log.d("TEST","문서 불러오기 에러");
+                            Log.d("TEST", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+        if(isLike) {
+            //누른거
+            holder.btnNoLike.setVisibility(View.GONE);
+            holder.btnLike.setVisibility(View.VISIBLE);
+            holder.tvLike.setText(likeNum +"명이 하트를 보냈어요");
+        }
+        else {
+            //안누른거
+            holder.btnNoLike.setVisibility(View.VISIBLE);
+            holder.btnLike.setVisibility(View.GONE);
+            holder.tvLike.setText(likeNum+"명이 하트를 보냈어요");
+        }
+    }
+
+    private void clickBtn() {
+        if(isLikePending) {
+            return;
+        }
+
+        if(timer != null) {
+            timer.cancel();
+        }
+
+        timer = new Timer();
+        Log.d("TEST","s");
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Log.d("TEST","ss");
+                upDateLikesToSever();
+                isLikePending = false;
+            }
+        },5000);
+        isLikePending = true;
+    }
+
+    private void upDateLikesToSever() {
+
+        DocumentReference likeRef = db.collection("likes").document(documentId);
+
+        Log.d("TEST","is"+isLike);
+        likeRef
+                .update("liked", isLike)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("TEST", "DocumentSnapshot successfully updated!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("TEST","업데이트 에러");
+                        Log.w("TEST", "Error updating document", e);
+                    }
+                });
+    }
+
+    private void makeSever(){
+        Map<String, Object> like = new HashMap<>();
+        like.put("userId",userId);
+        like.put("postId",postId);
+        like.put("liked",isLike);
+
+        db.collection("likes")
+                .add(like)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TEST", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        documentId = documentReference.getId();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("TEST","문서 생성 에러");
+                        Log.w("TEST", "Error adding document", e);
+                    }
+                });
     }
 
     @Override
