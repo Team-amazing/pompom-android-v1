86cba816ff8b0a016d978d910cf631f27901f8d6
parkUiery <parkuiery@gmail.com>
2023-09-19 22:54:57 +0900

    🌱 :: 상세 보기 데이터

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/community/detail/Comment.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/community/detail/Comment.java
@@ -1,2 +1,41 @@
-package com.amazing.android.autopompomme.community.detail;public class Comment {
+package com.amazing.android.autopompomme.community.detail;
+
+public class Comment {
+    private String userId;
+    private String comment;
+    private long timestamp;
+
+    public Comment() {
+
+    }
+
+    public Comment(String userId,String comment, long timestamp) {
+        this.userId = userId;
+        this.comment = comment;
+        this.timestamp = timestamp;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
