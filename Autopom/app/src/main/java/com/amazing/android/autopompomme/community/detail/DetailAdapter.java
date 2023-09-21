dabef1b523ae945b7e51deae5defea9846c99bf5
parkUiery <parkuiery@gmail.com>
2023-09-21 12:42:57 +0900

    🌱 :: 상세 화면 등록 이미지 리사이클러뷰

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/community/detail/DetailAdapter.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/community/detail/DetailAdapter.java
@@ -1,2 +1,63 @@
-package com.amazing.android.autopompomme.community.detail;public class DetailAdapter {
+package com.amazing.android.autopompomme.community.detail;
+
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.amazing.android.autopompomme.R;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.ViewHolder> {
+
+    private List<String> imgUrls;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.iv_detail_img);
+        }
+
+        public ImageView getImageView() {
+            return imageView;
+        }
+    }
+
+    public DetailAdapter (List<String> imgUrls) {
+        this.imgUrls = imgUrls;
+        Log.d("TEST","im"+imgUrls);
+    }
+
+    @NonNull
+    @Override
+    public DetailAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.detail_img_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DetailAdapter.ViewHolder holder, int position) {
+        Uri imgUrl = Uri.parse(imgUrls.get(position));
+        Log.d("TEST","postImg"+imgUrl);
+
+        Glide.with(holder.imageView.getContext())
+                .load(imgUrl)
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return imgUrls.size();
+    }
 }
