e826908fb1645fa14fd7da5956447ae19020d5bd
parkUiery <parkuiery@gmail.com>
2023-09-22 11:13:20 +0900

    ♻ :: 폐키지 변경

---
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/ProfileFragment.java
@@ -0,0 +1,46 @@
+package com.amazing.android.autopompomme.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.amazing.android.autopompomme.R;
+import com.amazing.android.autopompomme.activity.SettingActivity;
+import com.amazing.android.autopompomme.databinding.FragmentProfileBinding;
+
+
+public class ProfileFragment extends Fragment {
+
+    FragmentProfileBinding binding;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentProfileBinding.inflate(getLayoutInflater());
+
+        binding.ibProfileSetting.setOnClickListener( v-> {
+            Intent intent = new Intent(getActivity(), SettingActivity.class);
+            startActivity(intent);
+        });
+
+        setData();
+
+        return binding.getRoot();
+    }
+
+    private void setData() {
+
+    }
+}
