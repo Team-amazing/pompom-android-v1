f709486cb95bd8b436273936401095a01cd9b6b3
parkUiery <parkuiery@gmail.com>
2023-09-23 15:49:34 +0900

    🌱 :: 프로필 관리 어댑터

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/ProfileAdapter.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/ProfileAdapter.java
@@ -1,2 +1,39 @@
-package com.amazing.android.autopompomme.profile;public class ProfileAdapter {
+package com.amazing.android.autopompomme.profile;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfileAdapter extends FragmentStateAdapter {
+
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private MyUploadFragment myUploadFragment;
+    private MyLikeFragment myLikeFragment;
+
+    public ProfileAdapter(Fragment fragment) {
+        super(fragment);
+    }
+
+    public void addFragment(Fragment fragment) {
+        fragmentList.add(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
 }
