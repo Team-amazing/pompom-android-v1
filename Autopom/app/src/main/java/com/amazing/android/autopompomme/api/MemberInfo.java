fa50ae18410b176b8316d9a051869709baba7021
parkUiery <parkuiery@gmail.com>
2023-09-26 22:29:49 +0900

    🌧 :: 프로필 이미지 추가

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/api/MemberInfo.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/api/MemberInfo.java
@@ -2,9 +2,11 @@ package com.amazing.android.autopompomme.api;
 
 public class MemberInfo {
     private String nickname;
+    private String userUri;
 
-    public MemberInfo(String nickname){
+    public MemberInfo(String nickname,String userUri){
         this.nickname = nickname;
+        this.userUri = userUri;
     }
 
     public String getNickname() {
@@ -13,5 +15,13 @@ public class MemberInfo {
 
     public void setNickname(String nickname){
         this.nickname = nickname;
+    }
+
+    public String getUserUri() {
+        return userUri;
+    }
+
+    public void setUserUri(String userUri) {
+        this.userUri = userUri;
     }
 }
