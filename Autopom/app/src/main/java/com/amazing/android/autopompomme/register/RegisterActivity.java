a643e5489a2b62796f930e93aa1499913cb11d81
parkUiery <parkuiery@gmail.com>
2023-09-28 23:57:42 +0900

    🌱 :: 식물 등록 페이지 추가

---
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.amazing.android.autopompomme.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amazing.android.autopompomme.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
