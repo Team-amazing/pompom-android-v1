96abec6879e9258a67c8040642a43fa210178616
parkUiery <parkuiery@gmail.com>
2023-10-01 19:33:15 +0900

    🌱 :: 랭킹 페이지 생성

---
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/ranking/RankingActivity.java
@@ -0,0 +1,16 @@
+package com.amazing.android.autopompomme.ranking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.amazing.android.autopompomme.R;
+
+public class RankingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+    }
+}
