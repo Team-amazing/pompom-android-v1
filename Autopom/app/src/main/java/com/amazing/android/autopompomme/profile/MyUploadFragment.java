7930be036ccfa5704d57e82e5f4e21795f80c0ef
parkUiery <parkuiery@gmail.com>
2023-10-03 10:52:46 +0900

    🌱 :: 내 업로드

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/MyUploadFragment.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/MyUploadFragment.java
@@ -2,65 +2,85 @@ package com.amazing.android.autopompomme.profile;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.amazing.android.autopompomme.R;
+import com.amazing.android.autopompomme.community.CommunityList;
+import com.amazing.android.autopompomme.databinding.FragmentMyUploadBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyUploadFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class MyUploadFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MyUploadFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyUploadFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyUploadFragment newInstance(String param1, String param2) {
-        MyUploadFragment fragment = new MyUploadFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    FragmentMyUploadBinding binding;
+    private FirebaseUser user;
+    private ArrayList<MyUploadList> arrayList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_upload, container, false);
+
+        binding = FragmentMyUploadBinding.inflate(getLayoutInflater());
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        int numberOfColumns = 3;
+        RecyclerView recyclerView = binding.rvMyUpload;
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),numberOfColumns));
+
+        arrayList = new ArrayList<>();
+
+        if(user != null) {
+            String uid = user.getUid();
+
+            db.collection("posts")
+                    .whereEqualTo("uid",uid)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if(task.isSuccessful()) {
+                                arrayList.clear();
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Log.d("TEST", document.getId() + " => " + document.getData());
+                                    MyUploadList data = document.toObject(MyUploadList.class);
+                                    data.setPostId(document.getId());
+                                    arrayList.add(data);
+                                }
+
+                                MyUploadAdapter myUploadAdapter = new MyUploadAdapter(arrayList,uid);
+                                recyclerView.setAdapter(myUploadAdapter);
+                            } else {
+                                //에러
+                            }
+                        }
+                    });
+
+        }
+
+        return binding.getRoot();
     }
 }
--- Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/MyUploadList.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/MyUploadList.java
@@ -1,2 +1,91 @@
-package com.amazing.android.autopompomme.profile;public class MyUploadList {
+package com.amazing.android.autopompomme.profile;
+
+import android.net.Uri;
+
+import java.util.List;
+
+public class MyUploadList {
+    private String postId;
+    private String profileName;
+    private Uri profileUri;
+    private String date;
+    private String title;
+    private List<String> postUri;
+    private String content;
+    private int likeNum;
+    private int commentNum;
+
+    public MyUploadList() {}
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public Uri getProfile() {
+        return profileUri;
+    }
+
+    public void setProfile(Uri profile) {
+        this.profileUri = profileUri;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getPostUri() {
+        return postUri;
+    }
+
+    public void setPostUri(List<String> postUri) {
+        this.postUri = postUri;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getLikeNum() {
+        return likeNum;
+    }
+
+    public void setLikeNum(int likeNum) {
+        this.likeNum = likeNum;
+    }
+
+    public int getCommentNum() {
+        return commentNum;
+    }
+
+    public void setCommentNum(int commentNum) {
+        this.commentNum = commentNum;
+    }
 }
