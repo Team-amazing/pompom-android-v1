2e29b8b6d405bc9980a7fc2be409c64043314ce6
parkUiery <parkuiery@gmail.com>
2023-10-05 23:07:12 +0900

    🌧 :: 랭킹 화면 추가

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/ranking/RankingActivity.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/ranking/RankingActivity.java
@@ -1,16 +1,45 @@
 package com.amazing.android.autopompomme.ranking;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.amazing.android.autopompomme.R;
+import com.amazing.android.autopompomme.databinding.ActivityRankingBinding;
+import com.amazing.android.autopompomme.databinding.ActivityRegisterBinding;
 
 public class RankingActivity extends AppCompatActivity {
 
+    ActivityRankingBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ranking);
+
+        binding = ActivityRankingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.tbRanking;
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
