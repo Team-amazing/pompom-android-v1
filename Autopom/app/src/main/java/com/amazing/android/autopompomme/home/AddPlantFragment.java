3580eadc1fc71d797f6dbf04767496829e191f9a
parkUiery <parkuiery@gmail.com>
2023-10-07 20:21:14 +0900

    ♻ :: 코드 정렬

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/home/AddPlantFragment.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/home/AddPlantFragment.java
@@ -2,14 +2,12 @@ package com.amazing.android.autopompomme.home;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.amazing.android.autopompomme.R;
+import androidx.fragment.app.Fragment;
+
 import com.amazing.android.autopompomme.databinding.FragmentAddPlantBinding;
 import com.amazing.android.autopompomme.register.RegisterActivity;
 
@@ -28,7 +26,7 @@ public class AddPlantFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        binding =  FragmentAddPlantBinding.inflate(inflater);
+        binding = FragmentAddPlantBinding.inflate(inflater);
 
         binding.ibAddPlant.setOnClickListener(v -> moveRegister());
         return binding.getRoot();
@@ -38,4 +36,5 @@ public class AddPlantFragment extends Fragment {
         Intent intent = new Intent(getActivity(), RegisterActivity.class);
         startActivity(intent);
     }
+
 }
