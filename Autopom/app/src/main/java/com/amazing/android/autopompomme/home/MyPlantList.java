caa1ebfa0ae6f53ede24e3db0aa6244b42fe8a73
parkUiery <parkuiery@gmail.com>
2023-10-07 20:23:30 +0900

    🌱 :: 식물 정보 리스트

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/home/MyPlantList.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/home/MyPlantList.java
@@ -1,2 +1,42 @@
-package com.amazing.android.autopompomme.home;public class MyPlantList {
+package com.amazing.android.autopompomme.home;
+
+public class MyPlantList {
+    private String plantSpecies;
+    private String plantNickName;
+    private String plantBirth;
+    private String plantImgUri;
+
+    public MyPlantList() {}
+
+    public String getPlantSpecies() {
+        return plantSpecies;
+    }
+
+    public void setPlantSpecies(String plantSpecies) {
+        this.plantSpecies = plantSpecies;
+    }
+
+    public String getPlantNickName() {
+        return plantNickName;
+    }
+
+    public void setPlantNickName(String plantNickName) {
+        this.plantNickName = plantNickName;
+    }
+
+    public String getPlantBirth() {
+        return plantBirth;
+    }
+
+    public void setPlantBirth(String plantBirth) {
+        this.plantBirth = plantBirth;
+    }
+
+    public String getPlantImgUri() {
+        return plantImgUri;
+    }
+
+    public void setPlantImgUri(String plantImgUri) {
+        this.plantImgUri = plantImgUri;
+    }
 }
