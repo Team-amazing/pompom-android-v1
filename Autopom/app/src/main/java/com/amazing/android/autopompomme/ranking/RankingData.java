2161999ffc372e8b2c930d7fce03113306f82343
parkUiery <parkuiery@gmail.com>
2023-10-14 20:30:16 +0900

    🌱 :: 랭킹 데이터

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/ranking/RankingData.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/ranking/RankingData.java
@@ -1,2 +1,21 @@
-package com.amazing.android.autopompomme.ranking;public class RankingData {
+package com.amazing.android.autopompomme.ranking;
+
+public class RankingData {
+    private String nickName;
+    private int score;
+
+    public RankingData() {}
+
+    public RankingData(String nickName, int score) {
+        this.nickName = nickName;
+        this.score = score;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
