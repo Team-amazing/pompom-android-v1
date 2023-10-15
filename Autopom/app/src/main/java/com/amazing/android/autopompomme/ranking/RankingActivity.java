a3fbf93eb11893b179c5cc9f5d6f85fa72579445
parkUiery <parkuiery@gmail.com>
2023-10-15 17:53:17 +0900

    🌧 :: 랭킹 불러오기 추가

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/ranking/RankingActivity.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/ranking/RankingActivity.java
@@ -1,21 +1,33 @@
 package com.amazing.android.autopompomme.ranking;
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.amazing.android.autopompomme.R;
+import com.amazing.android.autopompomme.api.MemberInfo;
 import com.amazing.android.autopompomme.databinding.ActivityRankingBinding;
-import com.amazing.android.autopompomme.databinding.ActivityRegisterBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RankingActivity extends AppCompatActivity {
 
     ActivityRankingBinding binding;
+    private FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +35,69 @@ public class RankingActivity extends AppCompatActivity {
         binding = ActivityRankingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        db = FirebaseFirestore.getInstance();
+
+        initToolbar();
+        fetchRanking();
+
+    }
+
+    private void initToolbar() {
         Toolbar toolbar = binding.tbRanking;
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true);
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void fetchRanking() {
+        db.collection("users")
+                .orderBy("score", Query.Direction.DESCENDING)
+                .limit(10)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        List<MemberInfo> rankingList = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                            MemberInfo memberInfo = document.toObject(MemberInfo.class);
+                            rankingList.add(memberInfo);
+                        }
+
+                        Log.d("TEST", "랭킹 불러오기 성공:" + rankingList);
+
+                        if (rankingList.size() == 1) {
+                            MemberInfo first = rankingList.get(0);
+                            binding.tvRankingFirstMan.setText(first.getNickname());
+                            binding.ivRankingFirst.setImageURI(Uri.parse(first.getUserUri()));
+                            binding.tvRankingFirstP.setText(first.getScore());
+                        }
+                        if (rankingList.size() == 2) {
+                            MemberInfo second = rankingList.get(1);
+                            binding.tvRankingSecondMan.setText(second.getNickname());
+                            binding.ivRankingSecond.setImageURI(Uri.parse(second.getUserUri()));
+                            binding.tvRankingSecondP.setText(second.getScore());
+                        }
+                        if (rankingList.size() == 3) {
+                            MemberInfo third = rankingList.get(2);
+                            binding.tvRankingThirdMan.setText(third.getNickname());
+                            binding.ivRankingThird.setImageURI(Uri.parse(third.getUserUri()));
+                            binding.tvRankingThirdP.setText(third.getScore());
+                        }
+                        if (rankingList.size() > 3) {
+                            List<MemberInfo> restUser = rankingList.subList(3, rankingList.size());
+                            Log.d("TEST", "4위 부터 랭킹:" + restUser);
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //실패
+                        Log.d("TEST", "랭킹 불러오기 실패");
+                    }
+                });
     }
 
     @SuppressLint("NonConstantResourceId")
