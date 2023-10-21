a1022bfe0fe532ff09efeb44a07d810f53f1a4cc
parkUiery <parkuiery@gmail.com>
2023-10-21 14:33:57 +0900

    ♻ :: 코드 정렬

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/home/MyPlantFragment.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/home/MyPlantFragment.java
@@ -1,23 +1,17 @@
 package com.amazing.android.autopompomme.home;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.amazing.android.autopompomme.R;
+import androidx.fragment.app.Fragment;
+
 import com.amazing.android.autopompomme.databinding.FragmentMyPlantBinding;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Date;
 
 
 public class MyPlantFragment extends Fragment {
@@ -26,10 +20,11 @@ public class MyPlantFragment extends Fragment {
     int plantState;
 
     MyPlantList arrayList;
+
     public MyPlantFragment(MyPlantList arrayList) {
         this.arrayList = arrayList;
-        Log.d("TEST","myPlant1/"+arrayList);
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,41 +41,48 @@ public class MyPlantFragment extends Fragment {
         setData();
         setProgress();
         setPlantImg();
+
         return binding.getRoot();
     }
 
     private void setPlantImg() {
-        switch ((int)plantState/10) {
+        switch ((int) plantState / 10) {
             case 0:
             case 1:
-                binding.ivMyPlantPot1.setVisibility(View.VISIBLE); break;
+                binding.ivMyPlantPot1.setVisibility(View.VISIBLE);
+                break;
             case 2:
             case 3:
-                binding.ivMyPlantPot2.setVisibility(View.VISIBLE); break;
+                binding.ivMyPlantPot2.setVisibility(View.VISIBLE);
+                break;
             case 4:
             case 5:
-                binding.ivMyPlantPot3.setVisibility(View.VISIBLE); break;
+                binding.ivMyPlantPot3.setVisibility(View.VISIBLE);
+                break;
             case 6:
             case 7:
-                binding.ivMyPlantPot4.setVisibility(View.VISIBLE); break;
+                binding.ivMyPlantPot4.setVisibility(View.VISIBLE);
+                break;
             case 8:
             case 9:
-                binding.ivMyPlantPot5.setVisibility(View.VISIBLE); break;
+                binding.ivMyPlantPot5.setVisibility(View.VISIBLE);
+                break;
             case 10:
-                binding.ivMyPlantPot6.setVisibility(View.VISIBLE); break;
+                binding.ivMyPlantPot6.setVisibility(View.VISIBLE);
+                break;
         }
     }
 
     private void setProgress() {
         int plantLife = 30;
-        plantState = 100/plantLife * Integer.parseInt(getDate());
+        plantState = 100 / plantLife * Integer.parseInt(getDate());
         binding.pbMyPlantState.setProgress(plantState);
-        binding.tvMyPlantState.setText(plantState+"%");
+        binding.tvMyPlantState.setText(plantState + "%");
     }
 
     private void setData() {
         binding.tvMyPlantName.setText(arrayList.getPlantNickName());
-        binding.tvMyPlantDate.setText("+"+getDate());
+        binding.tvMyPlantDate.setText("+" + getDate());
     }
 
     private String getDate() {
@@ -91,6 +93,6 @@ public class MyPlantFragment extends Fragment {
 
         LocalDate today = LocalDate.now();
 
-        return String.valueOf(ChronoUnit.DAYS.between(localDate, today)+1);
+        return String.valueOf(ChronoUnit.DAYS.between(localDate, today) + 1);
     }
 }
