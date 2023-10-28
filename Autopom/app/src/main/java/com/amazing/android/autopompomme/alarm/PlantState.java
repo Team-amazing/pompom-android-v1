61bf8f0fe287cbda7835822e9caa8fca3add0215
parkUiery <parkuiery@gmail.com>
2023-10-28 22:11:00 +0900

    🌱 :: 식물 상태

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/alarm/PlantState.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/alarm/PlantState.java
@@ -1,2 +1,14 @@
-package com.amazing.android.autopompomme.alarm;public class PlantStase {
+package com.amazing.android.autopompomme.alarm;
+
+public class PlantState {
+    private Boolean plantState;
+
+    public PlantState() {}
+    public Boolean getPlantState() {
+        return plantState;
+    }
+
+    public void setPlantState(Boolean plantState) {
+        this.plantState = plantState;
+    }
 }
