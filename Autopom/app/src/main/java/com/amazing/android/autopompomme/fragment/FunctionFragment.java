9e3f095ca572c3c01c996ce92fb6135163844fb0
parkUiery <parkuiery@gmail.com>
2023-10-30 13:26:58 +0900

    🌧 :: 온습도 데이터 가져오기

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/fragment/FunctionFragment.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/fragment/FunctionFragment.java
@@ -1,7 +1,9 @@
 package com.amazing.android.autopompomme.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,58 +11,56 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.amazing.android.autopompomme.R;
+import com.amazing.android.autopompomme.databinding.FragmentFunctionBinding;
+import com.amazing.android.autopompomme.linking.LinkingActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FunctionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FunctionFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FunctionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FunctionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FunctionFragment newInstance(String param1, String param2) {
-        FunctionFragment fragment = new FunctionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    FragmentFunctionBinding binding;
+    DatabaseReference dbRef;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_function, container, false);
+
+        binding = FragmentFunctionBinding.inflate(getLayoutInflater());
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Temp/hum");
+        initTemp();
+        linkBlueTooth();
+
+        return binding.getRoot();
+    }
+
+    private void linkBlueTooth() {
+        binding.ivFunctionBlueTooth.setOnClickListener(view -> {
+            Intent intent = new Intent(getActivity(), LinkingActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void initTemp() {
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                snapshot.getValue();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
