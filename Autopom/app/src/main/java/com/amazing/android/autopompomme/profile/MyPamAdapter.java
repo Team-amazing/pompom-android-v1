4fc06fbcf556d9168e1fba25ab4f33ea989bd57e
parkUiery <parkuiery@gmail.com>
2023-10-30 22:50:19 +0900

    🌧 :: 내 팜팜이 보기 추가

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/MyPamAdapter.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/MyPamAdapter.java
@@ -1,2 +1,63 @@
-package com.amazing.android.autopompomme.profile;public class MyPamAdapter {
+package com.amazing.android.autopompomme.profile;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.amazing.android.autopompomme.R;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class MyPamAdapter extends RecyclerView.Adapter<MyPamAdapter.ViewHolder> {
+
+    private ArrayList<MyPamList> arrayList;
+
+    public MyPamAdapter (ArrayList<MyPamList> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        TextView species;
+        TextView birth;
+        TextView nick;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.iv_my_pam_plant);
+            species = itemView.findViewById(R.id.tv_my_pam_species);
+            birth = itemView.findViewById(R.id.tv_my_pam_birth);
+            nick = itemView.findViewById(R.id.tv_my_pam_nick);
+        }
+    }
+    @NonNull
+    @Override
+    public MyPamAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_my_pam,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyPamAdapter.ViewHolder holder, int position) {
+        Glide.with(holder.imageView)
+                        .load(arrayList.get(position).getPlantImgUri())
+                        .into(holder.imageView);
+        //holder.imageView.setImageURI(Uri.parse(arrayList.get(position).getPlantImgUri()));
+        holder.species.setText("팜팜이 종 : "arrayList.get(position).getPlantSpecies());
+        holder.nick.setText("팜팜이 별명 : "arrayList.get(position).getPlantNickName());
+        holder.birth.setText("팜팜이 생일 : "arrayList.get(position).getPlantBirth());
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList != null ? arrayList.size() : 0;
+    }
 }
--- Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/MyPamList.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/profile/MyPamList.java
@@ -1,2 +1,50 @@
-package com.amazing.android.autopompomme.profile;public class MyPamList {
+package com.amazing.android.autopompomme.profile;
+
+public class MyPamList {
+    private String plantBirth;
+    private String plantImgUri;
+    private String plantNickName;
+    private String plantSpecies;
+    private String uid;
+
+    public MyPamList() {}
+    public String getPlantBirth() {
+        return plantBirth;
+    }
+
+    public String getPlantImgUri() {
+        return plantImgUri;
+    }
+
+    public String getPlantNickName() {
+        return plantNickName;
+    }
+
+    public String getPlantSpecies() {
+        return plantSpecies;
+    }
+
+    public void setPlantBirth(String plantBirth) {
+        this.plantBirth = plantBirth;
+    }
+
+    public void setPlantImgUri(String plantImgUri) {
+        this.plantImgUri = plantImgUri;
+    }
+
+    public void setPlantNickName(String plantNickName) {
+        this.plantNickName = plantNickName;
+    }
+
+    public void setPlantSpecies(String plantSpecies) {
+        this.plantSpecies = plantSpecies;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
 }
