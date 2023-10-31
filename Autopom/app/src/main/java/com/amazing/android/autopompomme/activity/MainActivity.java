ef5235ee516fb6716da60f3f699895336f9784b1
parkUiery <parkuiery@gmail.com>
2023-10-31 21:25:11 +0900

    🌧 :: 백그라운드 알림으로 변경

--- Autopom/app/src/main/java/com/amazing/android/autopompomme/activity/MainActivity.java
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/activity/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void startAlarmService() {
         Intent intent = new Intent(this, AlarmService.class);
-        startForegroundService(intent);
+        startService(intent);
     }
 
     private void initAlarm() {
