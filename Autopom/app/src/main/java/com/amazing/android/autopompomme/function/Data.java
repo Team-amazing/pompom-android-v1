6632d3da936c18be93b011e55b486268ded27dfb
parkUiery <parkuiery@gmail.com>
2023-11-03 21:28:48 +0900

    🌱 :: 하드웨어 통신 데이터

---
+++ Autopom/app/src/main/java/com/amazing/android/autopompomme/function/Data.java
@@ -0,0 +1,24 @@
+package com.amazing.android.autopompomme.function;
+
+public class Data {
+    private int tem;
+    private int hum;
+
+    public Data() {}
+
+    public int getHum() {
+        return hum;
+    }
+
+    public int getTem() {
+        return tem;
+    }
+
+    public void setHum(int hum) {
+        this.hum = hum;
+    }
+
+    public void setTem(int tem) {
+        this.tem = tem;
+    }
+}
